5a06289c07b4bece5dd9c69aac6457ee525e913f
Simao Pedro <spedro@thoughtworks.com>
2019-08-06 11:32:14 -0300

    BRV-0000 [Simao] Working on Adapter

---
+++ app/src/main/java/myapplcation/simonsapp/com/mentoriaandroidprojeto01/AnimalsAdapter.kt
@@ -0,0 +1,25 @@
+package myapplcation.simonsapp.com.mentoriaandroidprojeto01
+
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+
+class AnimalsAdapter : BaseAdapter() {
+
+    override fun getCount(): Int {
+        return 0;
+    }
+
+    override fun getItem(position: Int): Any? {
+        return null;
+    }
+
+    override fun getItemId(position: Int): Long {
+        return 0;
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View? {
+        return null;
+    }
+
+}
